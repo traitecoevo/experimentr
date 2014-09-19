1c661d3aa7fc43bc598c50856e1668a5ec96105e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2014-09-19 14:31:17 +1000

    Remove duplication in runner code
    Fixes #14

--- R/run.R
+++ R/run.R
@@ -7,7 +7,10 @@ experimentr_options <- function() {
     make_option(c("-t", "--task"), type="character",
                 help="Task within experiment to run"),
     make_option(c("-i", "--id"), type="integer",
-                help="ID of the job to run"))
+                help="ID of the job to run"),
+    make_option(c("-n", "--dry-run"), type="logical",
+                default=FALSE, dest="dry_run",
+                help="Dry run (don't actually run anything)"))
 }
 
 ##' Run an experiment, from the command line.
@@ -16,41 +19,21 @@ experimentr_options <- function() {
 ##' @title Run an experiment, from the command line
 ##' @importFrom optparse parse_args
 ##' @param args Arguments (or NULL to read from R's \code{commandArgs})
-##' @param dry_run Just print information but don't run anything
 ##' @export
 ##' @importFrom optparse OptionParser
-main <- function(args=NULL, dry_run=FALSE) {
+main <- function(args=NULL) {
   if (is.null(args)) {
     args <- parse_args(OptionParser(option_list=experimentr_options()))
   }
   experiment <- args$experiment
   task       <- args$task
   id         <- args$id
-
-  message("Experiment: ", experiment)
-  message("Task:       ", task)
-  message("Id:         ", id)
-
-  dat <- yaml::yaml.load_file(experiments_filename())
-  pars <- load_task_info(experiment, task, id, dat)
-  env <- create_environment(experiment, task, dat)
-
-  if (!dry_run) {
-    save_metadata(experiment, task, id, dat, env)
-    run(pars, env)
+  dry_run    <- args$dry_run
+  if (length(id) != 1L) {
+    stop("Expected single id")
   }
-}
 
-run <- function(pars, env) {
-  message("--- Starting at ", Sys.time())
-  f <- get(pars$function_name, env, mode="function")
-  nms <- names(formals(f))
-  ## TODO: Process prerequisites here to allow using filenames from
-  ## previous versions.
-  backup(pars$filename, TRUE)
-  ret <- do.call(f, pars[names(pars) %in% nms], envir=env)
-  saveRDS(ret, pars$filename)
-  message("--- Finishing at ", Sys.time())
+  run_task(experiment, task, id, dry_run=dry_run)
 }
 
 ##' Run a task.  This is designed to run locally, rather than being
@@ -65,16 +48,24 @@ run <- function(pars, env) {
 ##' particular, it will probably be useful to set
 ##' \code{mc.preschedule=FALSE} and specify \code{mc.cores}.
 ##' @export
-run_task <- function(experiment, task, id=NULL, parallel=TRUE, ...) {
+run_task <- function(experiment, task, id=NULL, parallel=TRUE, ...,
+                     dry_run=FALSE) {
+  message("Experiment: ", experiment)
+  message("Task:       ", task)
+  
   dat <- yaml::yaml.load_file(experiments_filename())
   if (is.null(id)) {
     id <- ids(experiment)
   }
+  message("Id:         ", paste(id, collapse=", "))
+  
   f <- function(id) {
     pars <- load_task_info(experiment, task, id, dat)
     env <- create_environment(experiment, task, dat)
-    save_metadata(experiment, task, id, dat, env)
-    run(pars, env)
+    if (!dry_run) {
+      save_metadata(experiment, task, id, dat, env)
+      run(pars, env)
+    }
   }
   if (parallel && length(id) > 1L) {
     parallel::mclapply(id, f, ...)
@@ -82,4 +73,16 @@ run_task <- function(experiment, task, id=NULL, parallel=TRUE, ...) {
     lapply(id, f)
   }
   invisible(NULL)
+}
+
+run <- function(pars, env) {
+  message("--- Starting at ", Sys.time())
+  f <- get(pars$function_name, env, mode="function")
+  nms <- names(formals(f))
+  ## TODO: Process prerequisites here to allow using filenames from
+  ## previous versions.
+  backup(pars$filename, TRUE)
+  ret <- do.call(f, pars[names(pars) %in% nms], envir=env)
+  saveRDS(ret, pars$filename)
+  message("--- Finishing at ", Sys.time())
 }
